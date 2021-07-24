f9acdc54b6021d874f6d098188df66c3e1b934e3
Yura Dukhno <yura.dukhno@gmail.com>
2021-07-24 11:52:41 +0300

    add search box clean on click and icon for search

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,29 +1,35 @@
 import React, { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
 import "../Layout/Main.css";
 
 export default function Main(props) {
   const [search, setSearch] = useState();
   const [newArr, setNewArr] = useState([]);
 
-  // Update the search value when user enter text in search box.
+  //! Update the search value when user enter text in search box.
   const handleOnChange = e => {
     setSearch(e.target.value);
   };
 
-  // Filter props.array to new array when user press Enter in search box.
+  //! Filter props.array to new array when user press Enter in search box.
   const handleOnKeyDown = e => {
     if (e.key === "Enter") {
       window.parent.postMessage({ type: "search", value: search }, "*");
     }
   };
 
+  //! Clear a search field.
+  const handleOnClick = e => {
+    setSearch("");
+  };
+
   useEffect(() => {
-    const msgEent = window.addEventListener("message", e => {
+    const msgEvent = window.addEventListener("message", e => {
       if (e.origin === "https://www.ikea.co.il") {
         setNewArr(e.data.results);
       }
     });
-    return () => window.removeEventListener("message", msgEent);
+    return () => window.removeEventListener("message", msgEvent);
   }, []);
 
   return (
@@ -32,11 +38,13 @@ export default function Main(props) {
         <input
           onKeyDown={handleOnKeyDown}
           onChange={handleOnChange}
+          onClick={handleOnClick}
           value={search}
           className="search"
           type="search"
           placeholder="Place text here"
         />
+        <FaSearch />
       </div>
       <div className="images__list">
         {newArr.length > 0
